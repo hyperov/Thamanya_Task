18917d8ae42d7b81b4e94b2fdd6a1630c9456ea8
Ahmed Nabil <a.ahmed.nabil90@gmail.com>
2026-02-28 06:14:37 +0200

    update square and grid types with sorting based on priority

--- app/src/main/java/com/nabil/ahmed/thamanyatask/home/view/HomeScreen.kt
+++ app/src/main/java/com/nabil/ahmed/thamanyatask/home/view/HomeScreen.kt
@@ -11,6 +11,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
@@ -19,10 +22,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.paging.compose.collectAsLazyPagingItems
-import androidx.paging.compose.itemKey
 import com.nabil.ahmed.thamanyatask.home.viewmodel.HomePaginationViewModel
 import com.nabil.ahmed.thamanyatask.home.viewmodel.HomeViewModel
 import com.nabil.ahmed.thamanyatask.ui.components.SquareComponent
+import com.nabil.ahmed.thamanyatask.ui.components.SquareWithTitlesAndSubtitles
 import com.nabil.ahmed.thamanyatask.utils.SectionViewType
 
 @Composable
@@ -39,25 +42,44 @@ fun HomeScreen(
     ) {
         items(
             sections.itemCount,
-//            key = sections.itemKey { index -> sections[index]?.let { "${it.name}_${it.order}" } ?: index }
         ) { index ->
             val section = sections[index]
             if (section != null) {
                 Column(modifier = Modifier.padding(horizontal = 16.dp)) {
+
                     Text(text = section.name)
                     Spacer(modifier = Modifier.height(8.dp))
+
                     when (section.type) {
                         SectionViewType.SQUARE.type ->
                             LazyRow(
                                 horizontalArrangement = Arrangement.spacedBy(12.dp),
                                 contentPadding = PaddingValues(horizontal = 0.dp)
                             ) {
-                                items(section.content) { content ->
+                                items(section.content.sortedBy { it.priority }) { content ->
                                     SquareComponent(
                                         content,
                                         modifier = Modifier.size(140.dp),
                                         onClick = {}
                                     )
+                                }
+                            }
+
+                        SectionViewType.TWO_LINES_GRID.type ->
+                            LazyHorizontalGrid(
+                                modifier = Modifier.height(140.dp),
+                                rows = GridCells.Fixed(2),
+                                horizontalArrangement = Arrangement.spacedBy(12.dp),
+                                verticalArrangement = Arrangement.spacedBy(16.dp),
+                            )
+                            {
+                                items(section.content.sortedBy { it.podcastPriority }) { content ->
+                                    SquareWithTitlesAndSubtitles(
+                                        imageUrl = content.avatarUrl,
+                                        title = content.name,
+                                        subtitle = content.description,
+                                        duration = content.duration,
+                                        onClick = {})
                                 }
                             }
                     }
--- app/src/main/java/com/nabil/ahmed/thamanyatask/ui/components/SquareWithTitlesAndSubtitles.kt
+++ app/src/main/java/com/nabil/ahmed/thamanyatask/ui/components/SquareWithTitlesAndSubtitles.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -42,7 +43,7 @@ fun SquareWithTitlesAndSubtitles(
 ) {
     Row(
         modifier = modifier
-            .fillMaxWidth()
+            .width(300.dp)
             .clickable(onClick = onClick),
         verticalAlignment = Alignment.Top
     ) {
